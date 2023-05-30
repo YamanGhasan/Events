78e3eb12e052bf4081b00aa477e8ad560304a519
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-05-30 13:00:15 +0300

    Create events.cs

---
+++ events.cs
@@ -0,0 +1,19 @@
+using system;
+
+namespace events
+{
+class program
+  {
+         static void Main(string[] args)
+            {
+
+            }
+}
+public class Stock
+     { 
+     private string name;
+     private decimal price;
+     
+     public string Name =>this.name;        //get property Name 
+     }
+}
