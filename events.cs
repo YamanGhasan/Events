71b9b832e1857ab2827b63351396b49f428b6a92
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-05-30 16:35:48 +0300

    Update events.cs

--- events.cs
+++ events.cs
@@ -1,87 +1,74 @@
 // events between 2 classes without UI
-using system;
+using System;
 
 namespace events
 {
-class program //subscriber  // recive or handle the event 
-  {
-         static void Main(string[] args)
-            {
- 
-           var stock = new Stock("Amazon");   // Creating a new Stock object with the name "Amazon"
-           stock.Price = 100;
-           
-          // Subscribe to the OnPriceChanged event
-          // When the OnPriceChanged event of the 'stock' object is raised, the 'Stock_OnPriceChanged' method  will be called to handle the event.
-          stock.OnPriceChanged += Stock_OnPriceChanged;  
-           
-            stock.changeStockPriceBy(0.05m);
-            stock.changeStockPriceBy(-0.02m);
-            stock.ChangeStockPriceBy(0.00m);
-            stock.OnPriceChanged -= Stock_OnPriceChanged; //     unsubscribe from the OnPriceChanged event
-           
-          // console.writeline($"Price before changing : ${stock.Price} ");
-           
-          // stock.changeStockPriceBy(0.05m);       // Changing the stock price by 5%    
-           
-         //  console.writeline($"Price after changing : ${stock.Price} ");
-           
-            }
-  
-  private static void  Stock_OnPriceChanged(stock stock ,decimal oldPrice) //method to call when event is published 
-        {
-             string result = "";
-                if (stock.Price > oldPrice)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    result = "up";
-                }
-                else if (oldPrice > stock.Price)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    result = "down";
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                }
-                Console.WriteLine($"{stock.Name}: ${stock.Price} - {result}");
-        }
-      Console.ReadLine();  // The program execution will pause at this line until the user enters a line of text and presses Enter.
-}
-  
-  
-  public delegate void stockPriceChangeHandler(stock stock ,decimal oldPrice);  // Delegate for handling stock price changes
-  
-public class Stock   //publisher who raises the event 
-     { 
-     private string name;
-     private decimal price;
-     
-     public event stockPriceChangeHandler OnPriceChanged; // Event triggered when the stock price changes
-
-  
-     public string Name =>this.name;        //get property Name 
-     public decimal Price{ get=> this.price ;set => this.price = value ;}    //setter & getter for price 
-  
-  
-  
-  
-  public Stock(string stockname)
-  {
-    this.name = stockname;           // the constructor initializes the name  property with the value  during object creation
-  }
-  
-  
-  public void changeStockPriceBy(decimal percent)
-  {
-    decimal oldPrice= this.price;  // value of old price 
-    this.price += math.Round(this.price * percent ,2); //the percentage by which the stock price should be changed
-    
-    if (OnPriceChanged != null) //make sure there is subscriber 
+    class Program // Subscriber - receives or handles the event
     {
-      OnPriceChanged(this,oldPrice);  // Publish or raise the event by invoking the event
+        static void Main(string[] args)
+        {
+            var stock = new Stock("Amazon"); // Creating a new Stock object with the name "Amazon"
+            stock.Price = 100;
+
+            // Subscribe to the OnPriceChanged event
+            // When the OnPriceChanged event of the 'stock' object is raised, the 'Stock_OnPriceChanged' method will be called to handle the event.
+            stock.OnPriceChanged += Stock_OnPriceChanged;
+
+            stock.ChangeStockPriceBy(0.05m);
+            stock.ChangeStockPriceBy(-0.02m);
+            stock.ChangeStockPriceBy(0.00m);
+            stock.OnPriceChanged -= Stock_OnPriceChanged; // Unsubscribe from the OnPriceChanged event
+        }
+
+        private static void Stock_OnPriceChanged(Stock stock, decimal oldPrice) // Method to call when event is published
+        {
+            string result = "";
+            if (stock.Price > oldPrice)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                result = "up";
+            }
+            else if (oldPrice > stock.Price)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                result = "down";
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            Console.WriteLine($"{stock.Name}: ${stock.Price} - {result}");
+        }
     }
-  }
-     }
+
+    public delegate void stockPriceChangeHandler(Stock stock, decimal oldPrice); // Delegate for handling stock price changes
+
+    public class Stock // Publisher that raises the event
+    {
+        private string name;
+        private decimal price;
+
+        public event stockPriceChangeHandler OnPriceChanged; // Event triggered when the stock price changes
+
+        public string Name => this.name; // Get property Name
+
+        public decimal Price { get => this.price; set => this.price = value; } // Setter & getter for price
+
+        public Stock(string stockname)
+        {
+            this.name = stockname; // The constructor initializes the name property with the value during object creation
+        }
+
+        public void ChangeStockPriceBy(decimal percent)
+        {
+            decimal oldPrice = this.price; // Value of old price
+            this.price += Math.Round(this.price * percent, 2); // The percentage by which the stock price should be changed
+
+            if (OnPriceChanged != null) // Make sure there is a subscriber
+            {
+                OnPriceChanged(this, oldPrice); // Publish or raise the event by invoking the event
+            }
+        }
+    }
 }
+
